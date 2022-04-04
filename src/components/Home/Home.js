1c7c35f3dd20a17b4c7d34155ae1b309121f0368
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-04 21:54:22 +0600

    firstPartReviews

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../Header/Header";
 import Shop from "../Shop/Shop";
 
 const Home = () => {
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import useReviews from "../../hooks/useReviews";
+import Review from "../Review/Review";
 
 const Reviews = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [reviews, setReviews] = useReviews();
+  return (
+    <div>
+      <h1 className="text-center text-xl font-semibold lg:text-5xl font-semibold">Our Customer Reviews</h1>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10 ">
+        {reviews.map((review) => (
+          <Review key={review.id} review={review}></Review>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
