2d693430011e4165fef33c55fcc5def18b97d6e1
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-05 01:00:57 +0600

    recharts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,6 +9,7 @@ import {
   LineChart,
   Line,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 const Dashboard = () => {
   const data = [
@@ -50,60 +51,69 @@ const Dashboard = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-2">
+    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 mt-10 dark:bg-gray-800 dark:border-gray-700">
       <div>
-        <h1 className="text-center">Invest VS Revenue</h1>
-        <AreaChart
-          width={850}
-          height={400}
-          data={data}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="month" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="investment"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="revenue"
-            stroke="#82ca9d"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
-        </AreaChart>
+        <h1 className="text-center lg:text-3xl font-semibold text-purple-500">
+          Investment <span className="text-sky-600"> VS </span>
+          <span className="text-green-500">Revenue</span>
+        </h1>
+        <ResponsiveContainer width="90%" height={300}>
+          <AreaChart
+            // width={850}
+            // height={400}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="investment"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
       <div>
-        <h1 className="text-center">Month Wise Sell</h1>
-        <LineChart
-          width={850}
-          height={400}
-          data={data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="sell" stroke="#8884d8" />
-        </LineChart>
+        <h1 className="text-center lg:text-3xl font-semibold text-purple-500">
+          Month Wise Sell
+        </h1>
+        <ResponsiveContainer width="90%" height={300}>
+          <LineChart
+            // width={850}
+            // height={400}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
