bb2b38d45bb69769d64f2547b468c1ca4c5e5b7d
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-05 02:11:03 +0600

    homeReviews

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import useReviews from "../../hooks/useReviews";
+import Review from "../Review/Review";
 import Shop from "../Shop/Shop";
 
 const Home = () => {
+  const [reviews, setReviews] = useReviews();
   return (
     <div>
       <Shop></Shop>
+      <h1 className="text-center text-xl font-semibold lg:text-5xl font-semibold">
+        Our Customer Reviews
+      </h1>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-20 mt-10">
+        {reviews.slice(0, 3).map((review) => (
+          <Review key={review.id} review={review}></Review>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -7,7 +7,7 @@ const Reviews = () => {
   return (
     <div>
       <h1 className="text-center text-xl font-semibold lg:text-5xl font-semibold">Our Customer Reviews</h1>
-      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10 ">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-20 mt-10 ">
         {reviews.map((review) => (
           <Review key={review.id} review={review}></Review>
         ))}
