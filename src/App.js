629efb7c6125cbe693fe40acb3afc53241293a5e
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-04 15:45:24 +0600

    fakeData

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Reviews from "./components/Reviews/Reviews";
 import Header from "./components/Header/Header";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
 function App() {
   return (
     <div className="App">
@@ -11,9 +14,9 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="/home" element={<Home></Home>} />
         <Route path="/reviews" element={<Reviews></Reviews>} />
-        <Route path="/dashboard" element={<Home></Home>} />
-        <Route path="/blogs" element={<Home></Home>} />
-        <Route path="/about" element={<Home></Home>} />
+        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+        <Route path="/about" element={<About></About>} />
       </Routes>
     </div>
   );
