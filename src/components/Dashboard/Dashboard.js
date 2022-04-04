5f0e80346fbed90e2ee27f4ecbd989d551c3e408
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-04 23:54:13 +0600

    recharts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,112 @@
-import React from 'react';
-
+import React from "react";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+  LineChart,
+  Line,
+  Legend,
+} from "recharts";
 const Dashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const data = [
+    {
+      month: "Mar",
+      investment: 100000,
+      sell: 241,
+      revenue: 104010,
+    },
+    {
+      month: "Apr",
+      investment: 200000,
+      sell: 423,
+      revenue: 245000,
+    },
+    {
+      month: "May",
+      investment: 500000,
+      sell: 726,
+      revenue: 670100,
+    },
+    {
+      month: "Jun",
+      investment: 500000,
+      sell: 529,
+      revenue: 454050,
+    },
+    {
+      month: "Jul",
+      investment: 600000,
+      sell: 601,
+      revenue: 509000,
+    },
+    {
+      month: "Aug",
+      investment: 700000,
+      sell: 670,
+      revenue: 810000,
+    },
+  ];
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        <h1 className="text-center">Invest VS Revenue</h1>
+        <AreaChart
+          width={850}
+          height={400}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="month" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="investment"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </div>
+      <div>
+        <h1 className="text-center">Month Wise Sell</h1>
+        <LineChart
+          width={850}
+          height={400}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+        </LineChart>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,13 +4,13 @@ const Review = (props) => {
   const { name, description, picture, rating } = props.review;
   return (
     <div>
-      <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img class="p-8 rounded-full" src={picture} alt="product image" />
+          <img className="p-8 rounded-full" src={picture} alt="product image" />
         </a>
-        <div class="px-5 pb-5">
+        <div className="px-5 pb-5">
           <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
               <span className="font-bold">Name: </span>
               {name}
             </h5>
@@ -19,16 +19,16 @@ const Review = (props) => {
             <span className="font-bold">Review: </span>
             {description}
           </p>
-          <div class="flex items-center mt-2.5 mb-5">
+          <div className="flex items-center mt-2.5 mb-5">
             <svg
-              class="w-5 h-5 text-yellow-400"
+              className="w-5 h-5 text-yellow-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
               {rating}
             </span>
           </div>
